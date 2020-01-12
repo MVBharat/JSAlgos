369f81330e080d79f791c1b7d71215edd8b134b5
MVBharat <manebharat85@gmail.com>
2020-01-13 00:15:41 +0530

    Add solution for steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,35 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// ======== Solution Method 1 =================
+
+function steps(n) {
+    for(let row=0; row < n; row++){
+        let stairs = ''
+       for(let column=0; column < n; column++){
+           if( column <= row){
+               stairs +='#'
+            }
+            else{
+                stairs +=' '
+            }
+        }
+        console.log(stairs);
+    }
+}
 
 module.exports = steps;
+
+// ======== Solution Method 2 =================
+
+// function steps(n, row=0, stair=''){
+//     if(n===row) return;
+    
+//     if(n===stair.length){
+//         console.log(stair);
+//         return steps(n, row+1) // go for next line
+//     }
+
+//     const add = stair.length <= row ? '#' : ' ';
+//     steps(n, row, stair+add);
+// }
