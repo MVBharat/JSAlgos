2f122a7cb15dccc9394c43ebf1a6059a9384f7a1
Bharat Mane <bharat.mane@1985.co.in>
2020-06-24 10:00:58 +0530

    Add Sorting and Event handlings

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's']
+    const counter = [0]
+
+    while(arr.length > 1){
+        const node =arr.shif();
+        if(node === 's'){
+            counter.push[0]
+            arr.push('s')
+        }else{
+            arr.push(...node.childen)
+            counter[counter.legth - 1]++;
+        }
+    }
+
+}
 
 module.exports = levelWidth;
